d5825df3a89df63bf66d5437bf38d15a5c5e238e
akmalariq <santoso54321@gmail.com>
2021-05-28 09:35:44 +0700

    add edit member function

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -12,10 +12,12 @@ class Admin extends CI_Controller
 
     public function index()
     {
-        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+        $data['user'] = $this->db->get_where('user', ['id' => $this->session->userdata('id')])->row_array();
         // echo "selamat datang " . $data['user']['name'];
 
         $data['title'] = "Dashboard";
+        $data['users'] = $this->db->get('user')->result_array();
+
 
         $this->load->view('templates/header', $data);
         $this->load->view('templates/sidebar', $data);
@@ -26,12 +28,13 @@ class Admin extends CI_Controller
 
     public function role()
     {
-        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
-        // echo "selamat datang " . $data['user']['name'];
+        $data['user'] = $this->db->get_where('user', ['id' => $this->session->userdata('id')])->row_array();
+        // var_dump($data['user']);
+        // die;
 
         $data['title'] = "Role";
 
-        $data['role'] = $this->db->get('user_role')->result_array();
+        $data['role'] = $this->db->get('user')->result_array();
 
         $this->load->view('templates/header', $data);
         $this->load->view('templates/sidebar', $data);
@@ -55,7 +58,7 @@ class Admin extends CI_Controller
         $this->load->view('templates/header', $data);
         $this->load->view('templates/sidebar', $data);
         $this->load->view('templates/topbar', $data);
-        $this->load->view('admin/role-access', $data);
+        $this->load->view('admin/role_access', $data);
         $this->load->view('templates/footer');
     }
 
@@ -78,5 +81,55 @@ class Admin extends CI_Controller
         }
 
         $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Access Changed!</div>');
+    }
+
+    public function edit()
+    {
+        $data['user'] = $this->db->get_where('user', ['id' => $this->input->get('id', True)])->row_array();
+
+        // Sets rules from form validation (Check the inputs if valid)
+
+        // Sets rules for name {trim: to remove trailing whitespace, required: the field cannot be empty}
+        $this->form_validation->set_rules('name', 'Name', 'required|trim');
+        // Sets rules for email {trim: to remove trailing whitespace, required: the field cannot be empty, valid_email: checks if the string is a valid email}
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email');
+
+        // Checks if form_validation has not run, shows login page. Else get input form
+        if ($this->form_validation->run() == false) {
+
+            $data['title'] = "Edit Member";
+
+            $this->load->view('templates/header', $data);
+            $this->load->view('templates/sidebar', $data);
+            $this->load->view('templates/topbar', $data);
+            $this->load->view('admin/edit', $data);
+            $this->load->view('templates/footer');
+        } else {
+            $newdata = [
+                // name is filtered through XSS filter and htmlspecialchars
+                'name' => htmlspecialchars($this->input->post('name', true)),
+
+                // email is filtered through XSS filter and htmlspecialchars
+                'email' => htmlspecialchars($this->input->post('email', true)),
+
+                // image use default until until further function is implemented (user can upload their own image)
+                // 'image' => "default.jpg",
+
+                // password is hashed
+                // 'password' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
+
+                // role_id is default to 2 for a user to be an admin, it needes to be referred by other admins
+                // 'role_id' => 2,
+
+                // is_active is default to 1 (active) until further function is implemented (user can authenticate their own email)
+                // 'is_active' => 1,
+
+            ];
+
+            $this->db->where('id', $data['user']['id']);
+            $this->db->update('user', $newdata);
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Profile has been updated</div>');
+            redirect('admin');
+        }
     }
 }
--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -4,6 +4,50 @@
     <!-- Page Heading -->
     <h1 class="h3 mb-4 text-gray-800"><?= $title ?></h1>
 
+    <div class="row">
+        <div class="col-lg-12">
+            <?= form_error('menu', '<div class="alert alert-danger" role="alert">', '</div>'); ?>
+            <?= $this->session->flashdata('message'); ?>
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Role</th>
+                        <th scope="col">Created At</th>
+                        <th scope="col">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php $i = 1 ?>
+                    <?php foreach ($users as $u) : ?>
+                        <tr>
+                            <th scope="row"><?= $i ?></th>
+
+                            <td><?= $u['name']; ?></td>
+
+                            <td><?= $u['email']; ?></td>
+
+                            <?php if ($u['role_id'] == 1) : ?>
+                                <td>Admin</td>
+                            <?php elseif ($u['role_id'] == 2) : ?>
+                                <td>Member</td>
+                            <?php endif; ?>
+
+                            <td><?= date('d F Y', $u['date_created']) ?></td>
+
+                            <td>
+                                <a href="<?= base_url("admin/edit/") . "?id=" . $u['id'] ?>" class="badge badge-success">edit</a>
+                                <a href="<?= base_url("admin/delete/") . $u['id'] ?>" class="badge badge-danger">delete</a>
+                            </td>
+                        </tr>
+                        <?php $i++ ?>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
 
 
 
